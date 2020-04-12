c87d64b6b2f428acb97f1a0a7a3ed967c6505b90
emerson07 <emerson.tamura07@gmail.com>
2020-04-11 23:29:20 -0300

    teste ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   SafeAreaView,
@@ -10,8 +10,28 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api'
+
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  },[])
   async function handleLikeRepository(id) {
+    const response = await api.post(`repositories/${id}/like`)
+    const likes = response.data.likes
+    const repositoriesUpdated = repositories.map((repository) => {
+      if (repository.id === id) {
+        return { ...repository, likes }
+      } else {
+        return repository
+      }
+    })
+    setRepositories(repositoriesUpdated);
+
     // Implement "Like Repository" functionality
   }
 
@@ -19,48 +39,49 @@ export default function App() {
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({ item: repository }) => (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
+              {repository.techs.map(tech =>
+                <View key={tech} style={styles.techsContainer}>
+                  <Text key={tech} style={styles.tech}>{tech}</Text>
+                </View>
+              )}
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${repository.id}`}
+                >
+                  {repository.likes} curtidas
+                </Text>
+              </View>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${repository.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        /> 
+          </SafeAreaView>
     </>
   );
 }
-
+          
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#7159c1",
   },
-  repositoryContainer: {
+    repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
     backgroundColor: "#fff",
@@ -82,12 +103,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     color: "#fff",
-  },
+        },
   likesContainer: {
     marginTop: 15,
     flexDirection: "row",
     justifyContent: "space-between",
-  },
+        },
   likeText: {
     fontSize: 14,
     fontWeight: "bold",
